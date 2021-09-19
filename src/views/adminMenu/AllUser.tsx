43cd15c0f8d24f6b664da049f1158fde368a6b04
Jin <jpar303@gmail.com>
2021-09-19 14:37:58 +0900

    fixed: minor issue

--- src/views/adminMenu/AllUser.tsx
+++ src/views/adminMenu/AllUser.tsx
@@ -5,34 +5,18 @@ import List from './components/List';
 const AllUser = () => {
 
   const searchData: (searchItemsType | searchItemsType[])[] = [
+   
     [
-      {
-        label: 'Date',
-        keyName: 'date',
-        type: 'DatePicker',
-      },
-
       {
         label: 'ID',
         keyName: 'id',
       },
 
-    ],
-
-    [
       {
         label: 'E-mail',
         keyName: 'senderEmail',
       },
-      {
-        label: 'Remittance',
-        keyName: 'remittanceMethod',
-        type: 'select',
-        datalist: [
-          { code: 0, value: '은행 계좌 입금' },
-          { code: 1, value: '모바일 월렛' },
-        ],
-      },
+      
     ],
 
   ];
